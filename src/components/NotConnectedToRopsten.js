2e7bb6808f15087ec65ba81a4c23c148f34ad4c5
Josh Matthew Talplacido <joshmatthewtalplacido@gmail.com>
2020-08-10 21:38:04 +0800

    Added Loading when starting

--- src/components/NotConnectedToRopsten.js
+++ src/components/NotConnectedToRopsten.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Web3Context } from '../context/Web3Context'
+import Loader from 'react-loader-spinner'
 
 // Show this component when connected to different network other than Ropsten
 class NotConnectedToRopsten extends Component {
@@ -14,9 +15,24 @@ class NotConnectedToRopsten extends Component {
 
     return (
       <Web3Context.Consumer>{context => {
+
+        let network = ''
+        if (context.network) {
+          network = context.network
+        }
+
         return (
           <div className="w-screen h-screen bg-blue-100 flex items-center justify-center bgg">
-            <h1 className="network">You are currently connected on {this.formatNetwork(context.network)}. Please switch to {this.formatNetwork("ropsten")} instead.</h1>
+            <>
+              {network === '' ? (
+                <Loader type="BallTriangle" color="#2a4365" height={85} width={85} />
+              ) : (
+                  <h1 className="network">
+                    You are currently connected on {this.formatNetwork(context.network)}.
+                    Please switch to {this.formatNetwork("ropsten")} instead.
+                  </h1>
+                )}
+            </>
           </div>
         )
       }}
